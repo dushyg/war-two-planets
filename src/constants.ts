bbf680825a47977623ed68e43f1c3d21a20fafa0
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-07-03 16:33:30 +0530

    Constants file added

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+export enum PLANET_NAMES {
+  'FALICORNIA',
+  'LENGABURU',
+}
+
+export enum FORCE_NAMES {
+  'HORSE',
+  'ELEPHANT',
+  'ARMOURED TANKS',
+  'SLING GUNS',
+}
+
+export enum FORCE_CODES {
+  'H',
+  'E',
+  'AT',
+  'SG',
+}
+
+export const ERRORS = {
+  noInputFilePath:
+    'Input not passed as process argument. Usage: node geektrust.js ./inputFilePath',
+};
