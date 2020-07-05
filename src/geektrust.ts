c4ceb1eb926df9aa6ba11e1d790593a99d0443b3
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-07-05 19:33:27 +0530

    consuming war class from App class

--- src/geektrust.ts
+++ src/geektrust.ts
@@ -1,7 +1,29 @@
+import { Army, WarResult } from './models';
+import { StateManager } from './state/stateManager';
+import { War } from './war';
+import { ArmyProvider } from './armyProvider/armyProvider';
+import { CombatantMatcher } from './combatantMatcher/combatantMatcher';
+
 class App {
-  public static start(): void {
-    // tslint:disable-next-line: no-console
-    console.log('Hello world!');
+  constructor(
+    private stateManager: StateManager,
+    private defenderArmyProvider: ArmyProvider,
+    private invaderArmyProvider: ArmyProvider,
+    private combatantMatcher: CombatantMatcher
+  ) {}
+  public start(): void {
+    try {
+      const war = new War(this.stateManager, this.combatantMatcher);
+      const warResult: WarResult = war.fight(
+        this.invaderArmyProvider.getArmy(),
+        this.defenderArmyProvider.getArmy()
+      );
+
+      // todo ouptut result to console
+    } catch (error) {
+      this.stateManager.setError(error);
+      throw error;
+    }
   }
 }
-App.start();
+// App.start();
