d16d7a32df75866973c558f28d20b67ff3408d14
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-07-05 19:32:19 +0530

    Adding getters and setter to stateManager

--- src/state/stateManager.ts
+++ src/state/stateManager.ts
@@ -8,5 +8,40 @@ export class StateManager {
     victor: '',
   };
 
-  constructor() {}
+  public getState(): State {
+    return { ...this.state };
+  }
+
+  public getVictor(): string {
+    return this.state.victor;
+  }
+
+  public getError(): string {
+    return this.state.error;
+  }
+
+  public setError(error: string): void {
+    this.setState({
+      ...this.state,
+      error,
+    });
+  }
+
+  public setVictor(victor: string): void {
+    this.setState({
+      ...this.state,
+      victor,
+    });
+  }
+
+  public setBattles(battles: Map<string, Battle>): void {
+    this.setState({
+      ...this.state,
+      battles: { ...battles },
+    });
+  }
+
+  private setState(updatedState: State): void {
+    this.state = updatedState;
+  }
 }
