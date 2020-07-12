bd7ac378274d0744433f89676a84304aeca8b85d
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-07-12 20:57:04 +0530

    nameBasedCombatantMatcher test

---
+++ src/combatantMatcher/nameBasedCombatantMatcher.spec.ts
@@ -0,0 +1,13 @@
+import { NameBasedCombatantMatcher } from './nameBasedCombatantMatcher';
+import { Combatant, Army } from '../models';
+
+describe('NameBasedCombatantMatcher', () => {
+  it('getDefender should return default object when no match is found', () => {
+    const combatant: Combatant = new NameBasedCombatantMatcher().getDefender(
+      {} as Combatant,
+      { forces: new Map<string, Combatant>() } as Army
+    );
+
+    expect(combatant).toMatchSnapshot();
+  });
+});
